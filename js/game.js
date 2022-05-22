6b7069e1f51944d55398cb6c2cef056cff51caf4
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-05-22 13:41:55 +0200

    move with arrows complete

---
+++ js/game.js
@@ -0,0 +1,51 @@
+const hero = document.querySelector('.hero');
+hero.style.top = '500px';
+hero.style.left = '100px';
+
+const ghost = document.querySelector('.ghost');
+ghost.style.top = '200px';
+ghost.style.left = '600px';
+
+const treasure = document.querySelector('.treasure');
+treasure.style.top = '0px';
+treasure.style.left = '400px';
+
+//function move hero depending on arrow by 1sqr
+
+let stepXaxis = 100
+let stepYaxis = 500
+const moveHero = (e) => {
+    console.log(e);
+    if(e.code === "ArrowRight"){
+        stepXaxis = stepXaxis + 100;
+        console.log(stepXaxis);
+        hero.style.left = `${stepXaxis}px`;
+        console.log(stepXaxis);
+    }
+    if(e.code === "ArrowLeft"){
+        stepXaxis = stepXaxis - 100;
+        console.log(stepXaxis);
+        hero.style.left = `${stepXaxis}px`;
+        console.log(stepXaxis);
+    }
+    if(e.code === "ArrowUp"){
+        stepYaxis = stepYaxis - 100;
+        console.log(stepYaxis);
+        hero.style.top = `${stepYaxis}px`;
+        console.log(stepYaxis);
+    }
+    if(e.code === "ArrowDown"){
+        stepYaxis = stepYaxis + 100;
+        console.log(stepYaxis);
+        hero.style.top = `${stepYaxis}px`;
+        console.log(stepYaxis);
+    }
+
+}
+
+
+
+
+
+addEventListener("keyup", moveHero)
+console.log(parseInt("100px"));
